c3fc768ff8ed75bd9c8a50a88ec7c85850e90c64
Faaiq Ali Khan <faiq.khan@onecarnow.com>
2025-01-13 18:37:35 +0100

    screen division

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:domain/domain.dart';
-import 'package:domain/repositories/open_weather_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
-import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
-import 'package:weather_app/blocs/home_screen/home_screen_bloc.dart';
 import 'package:weather_app/dependency_injection/dependency_injection.dart';
-import 'package:weather_app/screens/home_screen.dart';
+import 'package:weather_app/screens/main_screen.dart';
 
 void main() {
   DependencyInjection.initDependencies();
@@ -23,54 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: "Weather Mobile Application"),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => HomeScreenBloc(
-        WeatherDetailsUseCase(
-          GetIt.I.get<OpenWeatherRepository>(),
-        ),
-      )..add(GetForecastData()),
-      child: Scaffold(
-        backgroundColor: Colors.white70,
-        body: SafeArea(
-          child: BlocBuilder<HomeScreenBloc, HomeScreenState>(
-            builder: (context, state) {
-              if (state is HomeScreenErrorState) {
-                return Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      context.read<HomeScreenBloc>().add(GetForecastData());
-                    },
-                    child: Text("Retry"),
-                  ),
-                );
-              }
-              if (state is HomeScreenLoadedState) {
-                return SmartRefresher(
-                  controller: context.read<HomeScreenBloc>().refreshController,
-                  onRefresh: () =>
-                      context.read<HomeScreenBloc>().add(RefreshForecastData()),
-                  child: HomeScreen(),
-                );
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-        ),
-      ),
+      home: const MainScreen(),
     );
   }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,53 @@
+import 'package:domain/domain.dart';
+import 'package:domain/repositories/open_weather_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
+import 'package:weather_app/blocs/home_screen/home_screen_bloc.dart';
+import 'package:weather_app/screens/home_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => HomeScreenBloc(
+        WeatherDetailsUseCase(
+          GetIt.I.get<OpenWeatherRepository>(),
+        ),
+      )..add(GetForecastData()),
+      child: Scaffold(
+        backgroundColor: Colors.white70,
+        body: SafeArea(
+          child: BlocBuilder<HomeScreenBloc, HomeScreenState>(
+            builder: (context, state) {
+              if (state is HomeScreenErrorState) {
+                return Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.read<HomeScreenBloc>().add(GetForecastData());
+                    },
+                    child: Text("Retry"),
+                  ),
+                );
+              }
+              if (state is HomeScreenLoadedState) {
+                return SmartRefresher(
+                  controller: context.read<HomeScreenBloc>().refreshController,
+                  onRefresh: () =>
+                      context.read<HomeScreenBloc>().add(RefreshForecastData()),
+                  child: HomeScreen(),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
